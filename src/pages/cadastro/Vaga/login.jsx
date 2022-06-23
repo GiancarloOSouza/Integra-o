7c0dada0d2f547c5ae041b89fe446197308ecd91
GiancarloOSouza <91104389+GiancarloOSouza@users.noreply.github.com>
2022-06-23 01:20:56 -0300

    integração login

--- src/pages/cadastro/Vaga/login.jsx
+++ src/pages/cadastro/Vaga/login.jsx
@@ -1,102 +1,73 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useState } from "react";
 import PageDefault from "../../../components/PageDefault";
 import png from '../../../assets/img/logo.png'
 import './style.css'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-import api from '../../../Api';
+import  api  from '../../../api';
 
 
 export const Login = () => {
 
-    // const userRef = useRef();
-    // const errRef = useRef();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    // const [errMsg, setErrMsg] = useState("")
-    // const [success, setSuccess] = useState(false)
 
-    // useEffect(() => {
-    //     useRef.current.focus();
-    // }, [])
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    // useEffect(() => {
-    //     setErrMsg('');
-    // }, [email, password])
 
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     console.log(email, password)
-    //     setEmail('');
-    //         setPassword('');
-    //     setSuccess(true)
-    // }
+        try {
+            const response = await api.post("/usuarioLogin", {
+                email,
+                senha: password,
+            });
 
-    const handleClickRegister = (values) => console.log(values);
+            console.log(response.data)
 
-    useEffect(() => {
-        api.get('usuario')
-        .then((res) => setEmail(res.data))
-        .catch((err) => {
-            console.error("ops! ocorreu um erro" + err);
-        })
-    }, [])
-
-    useEffect(() => {
-        api.get('usuario')
-        .then((res) => setPassword(res.data))
-        .catch((err) => {
-            console.error("ops! ocorreu um erro" + err);
-        })
-    }, [])
+            if (response.status === 200) {
+                localStorage.setItem("email", response.data.usuario.email);
+                localStorage.setItem("token", response.data.token);
+                // localStorage.setItem("senha", response.data.Usuario.senha);
+                navigate("/");
+            } else {
+                alert("Erro ao realizar o login, check suas credenciais");
+            }
+        } catch (error) {
+            alert("Erro ao realizar o login, por favor tente novameante mais tarde");
+        }
+    }
 
     return (
         <PageDefault>
             <section>
-                {/* <p ref={errRef} className={errMsg ? "errmsg":
-                "offscreen"} aria-live="assertive">{errMsg}</p> */}
                 <div className="container-login">
                     <div className="wrap-login">
-                    {/* onSubmit={handleSubmit} */}
-                        <form  className="login-form">
+
+                        <form className="login-form" onSubmit={handleSubmit}>
                             <span className="title">Bem Vindo!!!</span>
                             <span className="title">
                                 <img src={png} alt="" />
                             </span>
 
                             <div className="wrap-input">
-                                {/* <input
-                                    //className={email !== "" ? 'has-val input' : 'input'}
-                                    type="text"
-                                    id="username"
-                                    ref={userRef}
-                                    autoComplete="off"
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    value={email}
-                                    required
-                                />
-                                <span className="focus-input" data-placeholder="Email"></span> */}
+
                                 <input
-                                    className={password !== "" ? 'has-val input' : 'input'}
+                                    className={email !== "" ? 'has-val input' : 'input'}
+                                    name="email"
                                     type="email"
                                     value={email}
                                     onChange={e => setEmail(e.target.value)}
                                 />
-
+                                <span className="focus-input" data-placeholder="Email"></span>
                             </div>
 
                             <div className="wrap-input">
-                            {/* <input
-                                    //className={email !== "" ? 'has-val input' : 'input'}
-                                    type="password"
-                                    id="password"
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    value={password}
-                                    required
-                                /> */}
+
                                 <input
                                     className={password !== "" ? 'has-val input' : 'input'}
+                                    name="senha"
                                     type="password"
                                     value={password}
                                     onChange={e => setPassword(e.target.value)}
@@ -121,5 +92,5 @@ export const Login = () => {
                 </div>
             </section>
         </PageDefault>
-    )
+    );
 };
